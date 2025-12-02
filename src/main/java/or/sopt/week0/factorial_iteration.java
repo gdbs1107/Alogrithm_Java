577085fe5bcc52f987c4410dbe5d55a85c3a6880
gdbs1107 <gdbs1107@naver.com>
2025-12-02 16:37:13 +0900

    재귀함수 이론 정리

---
+++ src/main/java/or/sopt/week0/factorial_iteration.java
@@ -0,0 +1,10 @@
+package or.sopt.week0;
+
+public class factorial_iteration {
+
+    /**
+     * https://www.acmicpc.net/problem/10872
+     * */
+
+
+}
---
+++ src/main/java/or/sopt/week0/factorial_recursion.java
@@ -0,0 +1,8 @@
+package or.sopt.week0;
+
+public class factorial_recursion {
+
+    public static void main(String[] args) {
+
+    }
+}
