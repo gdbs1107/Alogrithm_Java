089c6b7cdf155f8db49029134fe6dbc97588ad6e
gdbs1107 <gdbs1107@naver.com>
2025-12-02 23:48:34 +0900

    재귀 공부

---
+++ src/main/java/or/sopt/week0/factorial_recursion_buffer.java
@@ -0,0 +1,41 @@
+package or.sopt.week0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class factorial_recursion_buffer {
+
+
+    /**
+     * 사용자의 입출력을 받는 로직에서
+     *
+     * BufferReader VS Scanner 어떤 차이가 있을까?
+     *
+     * BufferReader는
+     *
+     * */
+
+    static Long recursion(Long in){
+
+        if (in <=1){
+            return 1L;
+        }else {
+            return in * recursion(in-1);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * 점화식: f(n) = f(n-1) + f(n-2)
+         * 기저조건: n=1 or n=0 일때 재귀가 종료된다
+         * */
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        long l = Long.parseLong(bufferedReader.readLine());
+
+        Long recursion = recursion(l);
+        System.out.println(recursion);
+    }
+}
