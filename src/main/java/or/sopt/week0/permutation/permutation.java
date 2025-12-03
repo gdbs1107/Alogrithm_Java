afec06caa540ad4b3c5af0088fcafcc8ec91b8ee
gdbs1107 <gdbs1107@naver.com>
2025-12-03 21:40:14 +0900

    배열

--- src/main/java/or/sopt/week0/permutation/permutation.java
+++ src/main/java/or/sopt/week0/permutation/permutation.java
@@ -3,19 +3,19 @@ package or.sopt.week0.permutation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class permutation {
 
     /**
      * https://www.acmicpc.net/problem/10974
+     *
      * */
 
     public static void main(String[] args) throws IOException {
 
         int[] arr;
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        Integer input = Integer.parseInt(rd.readLine());
+        int input = Integer.parseInt(rd.readLine());
 
         arr = new int[input];
 
