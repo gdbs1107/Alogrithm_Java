8bb7d8b00c32f2ff122bd61e8c3ec93a219e7d03
gdbs1107 <gdbs1107@naver.com>
2025-12-03 21:56:37 +0900

    모르겠다 ㅋㅋ

--- src/main/java/or/sopt/week0/permutation/permutation.java
+++ src/main/java/or/sopt/week0/permutation/permutation.java
@@ -13,13 +13,14 @@ public class permutation {
 
     public static void main(String[] args) throws IOException {
 
+        // 배열을 선언합니다. 리스트는 메모리와 속도 측면에서 불리합니다.
         int[] arr;
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(rd.readLine());
 
         arr = new int[input];
 
-        // input 만큼의 배열을 만들어야함
+        // input 만큼의 배열을 만들어야함 -> 0번 인덱스부터 1로 채웁니다 -> 자연스럽게 오름차순 배열이 완성
         for(int i=0; i<input; i++){
             arr[i]= i+1;
         }
@@ -29,16 +30,25 @@ public class permutation {
 
 
 
-    static void per(int[] arr,int depth, int n, int r ){
-        if(depth == r){
-            print(arr,r);
+    /**
+     *
+     * @Param arr: 목표 배열입니다.
+     * @Param depth: 현재 탐색중인 인덱스를 의미합니다.
+     * @Param length: 배열의 총 길이를 나타냅니다.
+     * @Param target: 순열의 길이를 나타냅니다.
+     * */
+    static void per(int[] arr,int depth, int length, int target){
+
+        // 기저조건 입니다. depth가 r이라는 뜻은
+        if(depth == target){
+            print(arr,target);
             return;
         }
 
 
-        for (int i =depth; i<n; i++){
+        for (int i =depth; i<length; i++){
             swap(arr,depth,i);
-            per(arr,depth+1,n,r);
+            per(arr,depth+1,length,target);
             swap(arr,depth,i);
 
         }
