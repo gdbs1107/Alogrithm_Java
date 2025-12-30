fe88d9f848260c9c797b96eb262a9a8f9c937d90
gdbs1107 <gdbs1107@naver.com>
2025-12-30 14:24:49 +0900

    StringBuilder

--- src/main/java/or/sopt/week1/팰린드롬_2트_10988.java
+++ src/main/java/or/sopt/week1/팰린드롬_2트_10988.java
@@ -12,6 +12,7 @@ public class 팰린드롬_2트_10988 {
         int len = input.length();
         int result = 1;
 
+        // StringBuilder로 단어를 reverse 시키면 StringBuilder 타입의 문자열을 반환하게 된다
         StringBuilder sb = new StringBuilder(input);
         sb.reverse();
 
