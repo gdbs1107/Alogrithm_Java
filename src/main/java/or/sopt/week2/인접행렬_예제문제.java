ad8f720fbdbc990db95750423d184c4231ba7969
gdbs1107 <gdbs1107@naver.com>
2026-01-06 13:07:57 +0900

    인접행렬

---
+++ src/main/java/or/sopt/week2/인접행렬_예제문제.java
@@ -0,0 +1,37 @@
+package or.sopt.week2;
+
+import java.io.*;
+import java.util.*;
+
+public class 인접행렬_예제문제 {
+
+    public static void main(String[] args) throws IOException{
+
+        int[][] arr = {
+                {0,1,1,0,0,0,0,0,0},
+                {1,0,0,0,0,0,0,0,0},
+                {1,0,0,1,0,0,0,0,0},
+                {0,0,1,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0}
+        };
+
+        int[] visited = new int[8];
+
+        for (int i=0; i<arr.length; i++){
+            for (int j=0; j<arr[i].length; j++){
+
+                if (arr[i][j]==1 && visited[i]==0){
+                    System.out.print(i);
+                    visited[i]=1;
+                    continue;
+                }
+
+            }
+        }
+
+    }
+}
---
+++ src/main/java/or/sopt/week2/인접행렬_예제문제_모범답안.java
@@ -0,0 +1,34 @@
+package or.sopt.week2;
+public class 인접행렬_예제문제_모범답안 {
+
+    // 이중 배열을 만들어두고
+    static int n = 10;
+    static int[][] g = new int[n][n];
+    static boolean[] visited = new boolean[n];
+
+    public static void main(String[] args) {
+
+        // 1번: 인접행렬 세팅
+        g[1][2] = g[2][1] = 1;
+        g[1][3] = g[3][1] = 1;
+        g[3][4] = g[4][3] = 1;
+
+        // 2번: 0번부터 방문 안 한 노드 찾고 DFS로 이어서 방문/출력
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    static void dfs(int v) {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        for (int next = 0; next < n; next++) {
+            if (g[v][next] == 1 && !visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
