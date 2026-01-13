73b5c43f67a62ab0c60046ecda423b5413dab8f9
gdbs1107 <gdbs1107@naver.com>
2026-01-13 16:02:20 +0900

    1436

---
+++ src/main/java/or/sopt/week2/문제/수학문제_2870.java
@@ -0,0 +1,43 @@
+package or.sopt.week2.문제;
+
+import java.util.*;
+import java.io.*;
+
+public class 수학문제_2870 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> result = new ArrayList<>();
+
+        for (int i=0; i<n; i++){
+            String input = br.readLine();
+
+            for (int j=0; j<input.length(); j++){
+                char c = input.charAt(j);
+
+                // 만약 숫자가 발견된다면
+                if (c > '0' && c < 'a'){
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(c);
+
+                    // 그 다음 문자들을 탐색하면서 숫자가 나올때 멈춘다
+                    for (int k = j; true; k++){
+                        char next = input.charAt(k);
+
+                        // 만약 다음문자가 알파벳이라면 -> break
+                        if (next>'a'){
+                            int temp = Integer.parseInt(sb.toString());
+                            result.add(temp);
+                            break;
+                        }
+                        // 그게 아니라면 숫자를 넣는다
+                        sb.append(next);
+                    }
+                }
+            }
+        }
+
+        System.out.print(result);
+    }
+}
