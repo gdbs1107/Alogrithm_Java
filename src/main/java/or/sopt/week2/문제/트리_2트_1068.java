2c001cfb62ca91ed7c5daba9370cb673d2c2026c
gdbs1107 <gdbs1107@naver.com>
2026-01-16 13:08:38 +0900

    트리'

---
+++ src/main/java/or/sopt/week2/문제/트리_2트_1068.java
@@ -0,0 +1,19 @@
+package or.sopt.week2.문제;
+
+import java.util.*;
+import java.io.*;
+
+public class 트리_2트_1068 {
+
+    static ArrayList<ArrayList<Integer>> adj;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        adj = new ArrayList<>
+        for (int i=0; i<n; i++){
+            adj
+        }
+    }
+
+}
