db32cf17b3e93376de830a8d9c7a610694ef1e36
gdbs1107 <gdbs1107@naver.com>
2026-02-03 14:46:41 +0900

    1316

---
+++ src/main/java/or/sopt/기초/week1/b_1316.java
@@ -0,0 +1,42 @@
+package or.sopt.기초.week1;
+
+import java.util.*;
+import java.io.*;
+
+public class b_1316 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i=0; i<n; i++){
+            boolean flag = true;
+            String input = br.readLine();
+            ArrayList<Character> list = new ArrayList<>();
+
+            for (int j=0; j<input.length(); j++){
+                char c = input.charAt(j);
+
+                if (j==0) {
+                    list.add(c);
+                    continue;
+                }
+
+                // 리스트의 가장 상단의 문자와 현재의 문자가 다르고 && 리스트가 그걸 포함하고 있다면 false
+                if (list.get(j-1)!=c && list.contains(c)){
+                    flag=false;
+                    break;
+                }
+                list.add(c);
+            }
+            if (flag){
+                result++;
+            }
+        }
+
+
+
+        System.out.print(result);
+    }
+}
---
+++ src/main/java/or/sopt/기초/week1/b_4673.java
@@ -0,0 +1,4 @@
+package or.sopt.기초.week1;
+
+public class b_4673 {
+}
