5f10b6a16907b71f7dca237c269076b2e33e8fbd
gdbs1107 <gdbs1107@naver.com>
2026-02-20 12:40:05 +0900

    14501

--- src/main/java/or/sopt/기초/b_2457.java
+++ src/main/java/or/sopt/기초/b_2457.java
@@ -1,2 +1,18 @@
-package or.sopt.기초;public class b_2457 {
+package or.sopt.기초;
+import java.util.*;
+import java.io.*;
+public class b_2457 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+
+        for (int i=0; i<5; i++){
+            int temp = Integer.parseInt(st.nextToken());
+            temp = temp*temp;
+            sum += temp;
+        }
+
+        System.out.print(sum&10);
+    }
 }
--- src/main/java/or/sopt/기초/b_2741.java
+++ src/main/java/or/sopt/기초/b_2741.java
@@ -1,2 +1,16 @@
-package or.sopt.기초;public class b_2741 {
+package or.sopt.기초;
+
+import java.util.*;
+import java.io.*;
+
+public class b_2741 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<n+1; i++){
+            System.out.println(i);
+        }
+    }
 }
