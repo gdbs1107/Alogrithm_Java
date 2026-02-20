d40c6722a59076b67163a40969e27adb0b53db98
gdbs1107 <gdbs1107@naver.com>
2026-02-20 13:45:40 +0900

    14501

--- src/main/java/or/sopt/기초/b_30802.java
+++ src/main/java/or/sopt/기초/b_30802.java
@@ -18,6 +18,8 @@ public class b_30802 {
         int t = Integer.parseInt(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
 
+        // 이걸 하지 말고 그냥 바로 결과값에 더해버리는게 나았음
+        // 딱 떨어지는 경우만 그냥 cnt로 보내고 그게 아닌 경우만 +1하는거로
         int[] shirts = new int[6];
         for (int i=0; i<6; i++){
             shirts[i] = (arr[i]+(t-1))/t;
