a1bb7d45eadbdad3f5a0e4b25d2aa73e251042f3
gdbs1107 <gdbs1107@naver.com>
2026-03-03 18:02:21 +0900

    .

---
+++ src/main/java/or/sopt/기초/b1463.java
@@ -0,0 +1,14 @@
+package or.sopt.기초;
+
+import java.util.*;
+import java.io.*;
+
+public class b1463 {
+
+    public static void Main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
+    }
+}
